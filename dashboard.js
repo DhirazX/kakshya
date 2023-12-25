14c628f9f1536b7409154536d4b867012ac3c87f
Dhiraj <dhrjpoudyal@gmail.com>
2023-12-25 14:05:12 +0545

    modal added

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,5 @@
 //dashboard animaion
-var records = document.querySelectorAll(".record");
+const records = document.querySelectorAll(".record");
 setTimeout(() => {
   records.forEach((record) => {
     record.classList.add("show-section");
@@ -7,5 +7,28 @@ setTimeout(() => {
 }, 300);
 
 records.forEach((record, index) => {
-  record.style.transitionDelay = 0.15 * index + "s";
+  record.style.transitionDelay = 0.1 * index + "s";
+});
+
+//for modal
+const openModal = document.querySelector(".create-btn");
+const closeModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
+//Recorder
+const recordBtn = document.querySelector(".record-btn");
+
+recordBtn.addEventListener("click", () => {
+  if (recordBtn.innerText == "Start") {
+    recordBtn.innerText = "Stop";
+  } else {
+    recordBtn.innerText = "Start";
+  }
 });
